a2b7a45819e1988a1aa7288a952694d79635b8f1
Гость <@B302-04.ygk.ru>
2024-12-10 14:33:36 +0300

    first commit

---
+++ api_demo_mamaeva/api_demo_mamaeva/FullNameSerializator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_demo_mamaeva
+{
+    internal class FullNameSerializator
+    {
+        public string value { get; set; }
+    }
+}
